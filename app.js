4802041a60d06461343a12714991226671af078c
jahnavreddy <jahnavredy12@gmail.com>
2024-01-20 19:32:15 +0530

    main

---
+++ app.js
@@ -0,0 +1,10 @@
+function ReactContainer(){
+  return (
+      <div>Hello! Welcome to Kalvium   
+         <div>Let's rock and roll</div>
+     </div> )
+  };  
+
+
+let  container = document.getElementById('react-container');
+ReactDOM.render(<ReactContainer />,container);    
